893509af9d001b47200894b406e21234b717f976
psamora <psamora@outlook.com>
2014-09-13 05:42:51 -0400

    Meal test

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://psamora:test@kahana.mongohq.com:10026/app29528023');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var accountSid = 'ACd882ca7c1db91ca067d5072ac3f0a5b8';
+var authToken = 'c64507802fc8ffd839ca321db09a827b';
+var url = require('url');
 
+var client = require('twilio')(accountSid, authToken);
 var app = express();
 app.use(bodyParser.json());
 app.set('view engine', 'jade')
@@ -20,34 +24,36 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-  var key = Math.ceil(parseInt(req.param("phone")) / 9999);
-  console.log(key);
-  res.status(key).end();
-});
 
-app.get('/confirm', function(req, res) {
-  var key = req.param("key");
-  var newPhone = req.param("phone");
-  if (key == Math.ceil(parseInt(newPhone) / 9999)) {
-	  var db = mongoose.connection;
-	  var hash = crypto.createHmac('sha1', "12345").update(newPhone).digest('hex');
-	  console.log(hash);
-	  var user = new User({phone: newPhone, id: hash});
-	  console.log(user.id);
-	  db.on('error', console.error.bind(console, 'connection error:'));
-	  db.once('open', function callback () {
-	  // yay!
-	  })
-
-	  user.save(function (err, user) {
+  var newPhone = req.param("phone")
+  var newUser = req.param("user")
+  if (typeof newPhone != 'undefined') {
+  	var salt = crypto.randomBytes(128).toString('base64');
+    var hash = crypto.createHmac('sha1', salt).update(newPhone).digest('hex')
+  	var url = "tr://" + hash
+  	console.log("login from " + newPhone + " hash: " + hash);
+	// client.messages.create({
+	// 	body: "Hey, thanks for using Tranquility! Click to confirm your login: " + url,
+	// 	to: newPhone,
+	// 	from: "+12673231393"
+	// 	}, function(err, message) {
+	// 	process.stdout.write(message.sid);
+	// })
+  }
+  else if (typeof newUser != 'undefined') {
+    var user = new User({id: hash});
+    user.save(function (err, user) {
 		  if (err) return console.error(err);
-	  });
-	res.status(user.id).end();
+	});
   }
   else {
-  	res.status("invalidkey").end();
+  	console.log("invalid login page access");
   }
-    });
+});
+
+app.get('/meal', function(req, res) {
+	console.log(req.query)
+})
 
 app.listen(process.env.PORT || 7002);
 
