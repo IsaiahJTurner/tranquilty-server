0e400a96bf223b9985ec8e9ddf2cb032cfb6e4d2
psamora <psamora@outlook.com>
2014-09-13 03:59:08 -0400

    Initial test

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://psamora:Shannahm3!@kahana.mongohq.com:10026/app29525528');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+var app = express();
+app.use(bodyParser.json());
+
+var usersSchema = mongoose.Schema({
+  	phone: String,
+  	id: String
+})
+
+var User = mongoose.model('user', usersSchema);
+
+app.get('/login', function(req, res) {
+  var key = Math.ceil(parseInt(req.param("phone")) / 9999);
+  console.log(key);
+  res.status(key).end();
+});
+
+app.get('/confirm', function(req, res) {
+  var key = req.param("key");
+  var newPhone = req.param("phone");
+  if (key == Math.ceil(parseInt(newPhone) / 9999)) {
+	  var db = mongoose.connection;
+	  var hash = crypto.createHmac('sha1', "12345").update(newPhone).digest('hex');
+	  console.log(hash);
+	  var user = new User({phone: newPhone, id: hash});
+	  console.log(user.id);
+	  db.on('error', console.error.bind(console, 'connection error:'));
+	  db.once('open', function callback () {
+	  // yay!
+	  })
+
+	  user.save(function (err, user) {
+		  if (err) return console.error(err);
+	  });
+	res.status(user.id).end();
+  }
+  else {
+  	res.status("invalidkey").end();
+  }
+    });
+
+app.listen(process.env.PORT || 7002);
+
+ console.log('Express server listening on port ' + app.get('port'));
